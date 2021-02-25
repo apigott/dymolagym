d3f92d7c2c9cc0066f754897176718decdd95bd8
apigott <aisling.pigott@colorado.edu>
2021-02-25 12:31:16 -0700

    stop import of FMU base envs in the environments module

--- modelicagym/environment/__init__.py
+++ modelicagym/environment/__init__.py
@@ -1,3 +1,5 @@
-from .modelica_base_env import ModelicaBaseEnv, ModelicaType, FMIStandardVersion
-from .modelica_cs_env import FMI1CSEnv, FMI2CSEnv, ModelicaCSEnv
-from .modelica_me_env import ModelicaMEEnv, FMI1MEEnv, FMI2MEEnv
+# from .modelica_base_env import ModelicaBaseEnv, ModelicaType, FMIStandardVersion
+# from .modelica_cs_env import FMI1CSEnv, FMI2CSEnv, ModelicaCSEnv
+# from .modelica_me_env import ModelicaMEEnv, FMI1MEEnv, FMI2MEEnv
+from .dymola_base_env import DymolaBaseEnv
+from .dymola_env import DymolaEnv
