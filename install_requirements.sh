03de664e398776890c560eec35caf39f8e341e19
apigott <34166094+apigott@users.noreply.github.com>
2022-03-17 10:06:33 -0600

    Create install_requirements.sh
    run this bash file to install dependencies

---
+++ install_requirements.sh
@@ -0,0 +1,2 @@
+pip install tensorboardx PyQt5 PyQt5-sip stable-baselines3 jupyter-core importlib-resources
+conda install --file conda_requirements.txt -c conda-forge -c conda-forge/label/gcc7 -c dsdale24
