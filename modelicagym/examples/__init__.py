e7c5453e4457ec543e0267ca4d22537c3b831e06
apigott <aisling.pigott@colorado.edu>
2022-03-21 11:50:16 -0600

    cleaned up

--- modelicagym/examples/__init__.py
+++ modelicagym/examples/__init__.py
@@ -1,13 +1,1 @@
-# from .cart_pole.cart_pole_q_learner import *
-# from .cart_pole.cart_pole_env import *
-# from .cart_pole.cart_pole_bdp import *
-# from .cart_pole.cart_pole_dqn_1 import run_dqn_experiments
-
-from .ieee9.ieee9_env import *
-from .ieee9.ieee9_eval import *
-
-from .ieee14.ieee14_env import *
-from .ieee14.ieee14_eval import *
-
-from .cu_campus import *
-from .cu_campus_pypower import *
+from .cartpole import *
